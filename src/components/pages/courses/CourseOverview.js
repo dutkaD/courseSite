a92e8442f8ea176cfcad7fee7f546a527764d6e2
Diana Dutka <diana.dutka@gmail.com>
2020-12-13 18:44:35 +0100

    routing to courses

--- src/components/pages/courses/CourseOverview.js
+++ src/components/pages/courses/CourseOverview.js
@@ -3,20 +3,20 @@ import '../../../App.css';
 import Footer from "../../Footer";
 import CourseTile from "./CourseTile";
 
-export default function Courses() {
+export default function CourseOverview() {
   return <div>
     <div className="container-fluid">
       <div className="row">
         <div className="col-sm column">
-          <CourseTile name="English" background="english_course"/>
+          <CourseTile name="English" path="english_course"/>
         </div>
         <div className="col-sm column">
-          <CourseTile name="Coding" background="coding_course"/>
+          <CourseTile name="Coding" path="coding_course"/>
         </div>
       </div>
       <div className="row">
         <div className="col-sm column">
-          <CourseTile name="English&Code" background="english_and_code"/>
+          <CourseTile name="English&Code" path="english_and_code"/>
         </div>
       </div>
     </div>
