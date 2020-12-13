cad3890000df5db24c88e6d2ad83d04a927e9cbf
Diana Dutka <diana.dutka@gmail.com>
2020-12-13 14:07:10 +0100

    fixed for deploy

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
-          <Route path='/services' component={Services} />
-          <Route path='/products' component={Products} />
+          <Route path='/courses' component={Services} />
+          <Route path='/aboutUs' component={Products} />
           <Route path='/sign-up' component={SignUp} />
         </Switch>
       </Router>
