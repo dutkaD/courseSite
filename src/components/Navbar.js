a2e2af6a82575c2c44c907c4d3070f5401c4d3fd
Diana Dutka <diana.dutka@gmail.com>
2020-12-13 17:44:19 +0100

    three course tiles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,6 +31,7 @@ function Navbar() {
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
             TNTM
             <i class='fab fa-typo3' />
+            Ficus
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
--- src/components/pages/courses/Courses.js
+++ src/components/pages/courses/Courses.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import '../../../App.css';
 import Footer from "../../Footer";
-import CourseSide from "./CourseSide";
+import CourseTile from "./CourseTile";
 
 export default function Courses() {
   return <div>
     <div className="container-fluid">
       <div className="row">
         <div className="col-sm column">
-          <CourseSide name="English"/>
+          <CourseTile name="English" background="english_course"/>
         </div>
         <div className="col-sm column">
-          <CourseSide name="Coding"/>
+          <CourseTile name="Coding" background="coding_course"/>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm column">
+          <CourseTile name="English&Code" background="english_and_code"/>
         </div>
       </div>
     </div>
