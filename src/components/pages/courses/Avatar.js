1b2e9d82f61b3f8499c1036905a82dcb58ba56af
Diana Dutka <diana.dutka@gmail.com>
2020-12-16 21:08:30 +0100

    alt

--- src/components/pages/courses/Avatar.js
+++ src/components/pages/courses/Avatar.js
@@ -4,7 +4,7 @@ export default function Avatar(props) {
     return <div>
         <div className="row">
             <div className="col-sm text-center">
-                <img src="images/anna.jpg" className="rounded-circle img-fluid avatar" alt="Photo of Anna"/>
+                <img src="images/anna.jpg" className="rounded-circle img-fluid avatar" alt="Anna - teacher at Ficus school"/>
             </div>
         </div>
         <h3 className="row justify-content-center">
