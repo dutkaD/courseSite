4d46dbf3a06853a1968365de7fb729024517533b
Diana Dutka <diana.dutka@zooplus.com>
2020-12-18 12:40:22 +0100

    fix links, add color vars

--- src/components/pages/courses/CourseTile.js
+++ src/components/pages/courses/CourseTile.js
@@ -4,7 +4,7 @@ import './CourseOverview.css';
 import {Link} from "react-router-dom";
 
 export default function CourseTile(props) {
-    return <Link className='' to={props.path}>
+    return <Link className='course-link' to={props.path}>
         <div className={`highlight d-flex fill align-items-center course_background justify-content-center 
                         ${props.path}`}>
             <h1 className='headline_course'>{props.name}</h1>;
