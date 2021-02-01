0be4113c5dcfd56caf72329c2fc2a61f6f96e48a
Diana Dutka <diana.dutka@zooplus.com>
2021-02-01 11:37:23 +0200

    .

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import '../../App.css';
-import Cards from './courses/cards/Cards';
-import HeroSection from '../HeroSection';
 import Footer from '../Footer';
 import Actual from "./courses/actual/Actual";
 
