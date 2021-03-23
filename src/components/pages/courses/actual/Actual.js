db60ca178d045a5c7471459b6c440d80dd19a409
Diana Dutka <diana.dutka@zooplus.com>
2021-03-23 13:39:29 +0100

    add different courses

--- src/components/pages/courses/actual/Actual.js
+++ src/components/pages/courses/actual/Actual.js
@@ -7,8 +7,71 @@ function Actual() {
 
     return (
         <div className="container">
-            <h1>Актуальные мероприятия </h1>
+
             <div className="row">
+                <h1>Предстояшие мероприятия</h1>
+            </div>
+
+            <div className="row">
+                <CourseCard
+                    image="images/code.jpeg"
+                    header="Курс 'Тише едешь - дальше будешь.' "
+                    title="Python без стресса."
+                    shortDescription="Курс из 12 занятий (*45 минут) расчитан на людей, которые не имеют опыта в изучении программировния."
+                    start="дата будет назначена после 01.05.2021"
+                    linkTo="/programming-for-all"
+                    buttonText="Полное описание курса"
+                    price="early birds (предоплата до 01.05) - 1800 грн., после 01.05 - 2999 грн."
+                    current={true}
+                />
+
+                <CourseCard
+                    image="images/code.jpeg"
+                    header="'Need For Speed' Python - интенсив."
+                    title="Курс по основам программирования на Python"
+                    shortDescription="Курс из 5 занятий (*120 минут) расчитан на людей, которые не имеют опыта в изучении программировния, но хотят "
+                    start="дата будет назначена после 01.05.2021"
+                    linkTo="/programming-for-all"
+                    buttonText="Полное описание курса"
+                    price="early birds (предоплата до 01.05) - 2000 грн., после 01.05 - 3200 грн."
+                    current={true}
+                />
+
+                <CourseCard
+                    image="images/coding.jpg"
+                    header="Workshop 'Что такое Git?'"
+                    title="2 часовой воркшоп по основам Git"
+                    shortDescription="Для участия в воркшопе не обязательные глубокие знания в программировании. Достаточно знать основные понятия."
+                    start="20.06.2021 - 19.00"
+                    linkTo="/what-is-git-workshop"
+                    buttonText="Детали и регистрация"
+                    price="200 грн."
+                    current={true}
+                />
+
+
+
+            </div>
+            <div className="row">
+                <CourseCard
+                    image="images/coding.jpg"
+                    header="Web Scraping Workshop"
+                    title="3 часовой воркшоп"
+                    shortDescription="Воркшоп рачитан на людей, которые уже имеют некий опыт с питоном."
+                    start="20.06.2021 - 19.00"
+                    linkTo="/what-is-git-workshop"
+                    buttonText="Детали и регистрация"
+                    price="150 грн."
+                    current={true}
+                />
+
+            </div>
+
+            <div className="row">
+                <h1>Прошедшие мероприятия</h1>
+            </div>
+            <div className="row">
+
                 <CourseCard
                     image="images/code.jpeg"
                     header="Programming for all"
@@ -16,57 +79,9 @@ function Actual() {
                     shortDescription="5-дневный интенсив при поддержке U.S. Department of State, Bureau of Educational and Cultural Affairs и American Councils for International Education."
                     start="01.03.2021"
                     linkTo="/programming-for-all"
-                    buttonText="Полное описание курса"
+                    buttonText="Полное описание курса / Отзывы"
                     price="бесплатно (после отбора самых мотивированых)"
                     current={true}
-                />
-
-                <CourseCard
-                    image="images/coding.jpg"
-                    header="5 занятий по Python для новичков"
-                    title="Базовый курс для гуманитариев."
-                    shortDescription="Курс из 5 занятий поможет безболезненно ворваться в IT. Основы программирования Python с практическими заданиями."
-                    start="07.05.2021"
-                    linkTo="/programming-for-all"
-                    buttonText="Детали и регистрация"
-                    price="от ₴2999"
-                    current={true}
-                />
-
-                <CourseCard
-                    image="images/speak.jpg"
-                    header="Fake It Until You Make It"
-                    title="Разговорный курс для образованного новичка"
-                    shortDescription="10-дневный интенсив по грамматике и фонетике научит создавать иллюзию идеального английского."
-                    start="19 апреля, 19:00"
-                    linkTo="/programming-for-all"
-                    buttonText="Полное описание курса"
-                    price="early birds (до 1 марта) —  ₴3500, после 1 марта — ₴4000"
-
-                />
-            </div>
-
-            <div className="row">
-                <CourseCard
-                    image="images/vet.jpg"
-                    header="Лекция ветеринара"
-                    title="How I Met Your Pet"
-                    shortDescription="Встреча с ветврачем + мини-словарик для практики английского в подарок "
-                    start="28 февраля, 11:00"
-                    linkTo="/programming-for-all"
-                    buttonText="Полное описание курса"
-                    price="₴0, предварительная регистрация обязательнаа"
-                />
-
-                <CourseCard
-                    image="images/silent.jpg"
-                    header="[Speaking] Silent Club"
-                    title="Разговорный клуб для скромных и немногословных"
-                    shortDescription="Просмотр и обсуждение фильма на английском в Telegram-чате"
-                    start="31 марта, 19:00"
-                    linkTo="/programming-for-all"
-                    buttonText="Узнать детали и зарегестрировться"
-                    price="₴0, предварительная регистрация обязательна "
                 />
             </div>
         </div>
